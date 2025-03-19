4a2d05bb3a185a24635863cf384ce7c778788378
Daniel <danielpenchev1709@gmail.com>
2025-03-19 15:19:40 +0200

    added login part with auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { AuthService } from './auth/auth.service';
+import { User } from './shared/user.model';
 
 @Component({
   selector: 'app-root',
@@ -15,27 +17,64 @@ export class AppComponent {
   isUserMenuOpen: boolean = false;
   isLoginPage: boolean = false;
   isRegisterPage: boolean = false;
+  isAuthtenticated: boolean = false;
+  user = new User();
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    route: ActivatedRoute
+  ) {
     this.subscribeToRouterEvents();
   }
 
-  private subscribeToRouterEvents() {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      this.isLoginPage = event.urlAfterRedirects === '/login';
-      this.isRegisterPage = event.urlAfterRedirects === '/register';
-    });
+  ngOnInit() {
+    //Remove user if token is not valid
+    this.authService.isTokenDataValid();
+
+    this.getUser(this.user);
   }
+
+  private subscribeToRouterEvents() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isLoginPage = event.urlAfterRedirects === '/login';
+        this.isRegisterPage = event.urlAfterRedirects === '/register';
+      });
+  }
+
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
-  toggleUserMenu(){
+  toggleUserMenu() {
     this.isUserMenuOpen = !this.isUserMenuOpen;
   }
 
   closeUserMenu() {
     this.isUserMenuOpen = false;
+  }
+  // Login Part
+
+  //Chatgpt says there is a better way to do it instead with ngDoCheck
+  ngDoCheck() {
+    this.isAuthtenticated = this.authService.isAuthenticated();
+    // Ако потребителят е автентикиран, обновяваме данните за него
+    if (this.isAuthtenticated) {
+      this.getUser(this.user);
+    }
+  }
+  onLogout() {
+    this.authService.loguout();
+    this.user = new User();
+  }
+  //Get current user
+  getUser(user) {
+    // Проверяваме дали има токен в localStorage
+    if (this.authService.isAuthenticated()) {
+      this.user.id = localStorage.getItem(this.authService.USER_ID);
+      this.user.userName = localStorage.getItem(this.authService.USER_NAME);
+      this.user.email = localStorage.getItem(this.authService.USER_EMAIL);
+    }
   }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  //https://localhost:7068
+  TOKEN_KEY = 'token';
+
+  BASE_URL = 'https://localhost:7068/';
+
+  VALID_TO = 'validTo';
+
+  USER_ID = 'userId';
+
+  USER_NAME = 'userName';
+
+  USER_EMAIL = 'email';
+
+  errorMessage: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(loginData) {
+    return this.http
+      .post(this.BASE_URL + 'account/GenerateToken', loginData)
+      .subscribe(
+        (res) => {
+          this.authenticate(res);
+          console.log("token res:",res);
+          
+        },
+        (err) => {
+          if (err.status == 0) {
+            this.errorMessage = 'Server error.';
+          }
+          if (err.status === 400) {
+            this.errorMessage = 'Please check you user name or password.';
+          } else {
+            this.errorMessage = err._body;
+          }
+        }
+      );
+  }
+  authenticate(res) {
+    this.errorMessage = '';
+
+    //HttpClient автоматично връща JSON обект.
+    // const authResponce = res.json();
+
+    const authResponce = res;
+    
+    if (authResponce !== null && authResponce.token) {
+      localStorage.setItem(this.TOKEN_KEY, authResponce.token);
+
+      localStorage.setItem(this.VALID_TO, authResponce.validTo);
+
+      localStorage.setItem(this.USER_ID, authResponce.user.id);
+
+      localStorage.setItem(this.USER_NAME, authResponce.user.userName);
+
+      localStorage.setItem(this.USER_EMAIL, authResponce.user.email);
+
+      this.router.navigate(['/']);
+    }
+  }
+  loguout() {
+    localStorage.removeItem(this.TOKEN_KEY);
+
+    localStorage.removeItem(this.VALID_TO);
+
+    this.router.navigate(['/']);
+  }
+
+  isAuthenticated() {
+    return localStorage.getItem(this.TOKEN_KEY) != null ? true : false;
+  }
+
+  isTokenDataValid() {
+    var dateNow = new Date();
+
+    var validToData = new Date(localStorage.getItem(this.VALID_TO));
+
+    if (validToData < dateNow) {
+      localStorage.removeItem(this.TOKEN_KEY);
+
+      localStorage.removeItem(this.VALID_TO);
+    }
+  }
+}
